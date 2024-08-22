ae2fdccfe2fa9758042340826e2b05c8891edefd
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2024-08-22 06:09:37 -0500

    feat(solucion.sql): add SQL queries to retrieve specific data related to collections, shipments, and lots for analysis and reporting purposes

--- solucion.sql
+++ solucion.sql
@@ -76,3 +76,55 @@ c."collectionDate"
 from subquery_maximo sm
   join cultivo."Collection" c
     on c."lotId" = sm.id and c.bunches = sm.max
+
+-- 7. Muestra la cantidad total de racimos recolectados (`Collection`) en cada lote (`Lot`) para el año en curso. Incluye el identificador del lote y la cantidad total de racimos.
+select 
+  l.id as "lotId",
+  l.name,
+  count(c.id) as "totalBunches"
+from cultivo."Lot" l
+  join cultivo."Collection" c
+    on c."lotId" = l.id
+where extract(year from c."collectionDate") = 2024
+group by l.id, l.name
+order by "totalBunches" desc;
+
+-- 8.    Encuentra los envíos (`Shipment`) que se realizaron para lotes (`Lot`) que han tenido más de 5 recogidas (`Collection`). Muestra el identificador del envío, el identificador del lote y la fecha del envío.
+select 
+  s.id as "shipmentId",
+  l.id as "lotId",
+  s."shipmentDate"
+from cultivo."Shipment" s
+  join cultivo."Collection" c
+    on c."shipmentId" = s.id
+  join cultivo."Lot" l
+    on c."lotId" = l.id
+group by s.id, l.id, s."shipmentDate"
+having count(c.id) > 5;
+
+-- 9.   Muestra el número total de recogidas (`Collection`) realizadas para cada lote (`Lot`) en el último año. Incluye el identificador del lote y el número total de recogidas.
+
+select 
+  l.id as "lotId",
+  count(c.id) as "totalCollections"
+from cultivo."Lot" l
+  join cultivo."Collection" c
+    on c."lotId" = l.id
+where extract(year from c."collectionDate") = 2023
+group by l.id
+order by "totalCollections" desc;
+
+-- 10. Encuentra los lotes (`Lot`) y los envíos (`Shipment`) que se realizaron en el mismo rango de fechas. Muestra el identificador del lote, el identificador del envío, y las fechas correspondientes.
+select 
+  l.id as "lotId",
+  s.id as "shipmentId",
+  s."shipmentDate",
+  c."collectionDate"
+from cultivo."Lot" l
+  join cultivo."Collection" c
+    on c."lotId" = l.id
+  join cultivo."Shipment" s
+    on s."shipmentDate" = c."collectionDate"
+order by l.id, s.id;
+
+
